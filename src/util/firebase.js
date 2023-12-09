7a16adf81dbc0fa756cc2bdfa9f9070d78cb4736
Changwenl <arthurlicw@gmail.com>
2023-12-09 18:45:39 +1100

    config

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from 'firebase/database';
+import firebaseConfig from './firebaseConfig';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBFROSYebvyH9wYwIPvl2AeiCFkPPpvv-8",
-  authDomain: "survey-backend-70299.firebaseapp.com",
-  databaseURL: "https://survey-backend-70299-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "survey-backend-70299",
-  storageBucket: "survey-backend-70299.appspot.com",
-  messagingSenderId: "83445893881",
-  appId: "1:83445893881:web:05487533d16000c520b028",
-  measurementId: "G-NGL342BKBL"
-};
 
 const firebaseApp = initializeApp(firebaseConfig);
 
