f29e7f28b405d3fa82ff581f8ccdeb3c39794c46
Changwenl <arthurlicw@gmail.com>
2024-03-26 01:39:34 +1100

    feat: modal overlay

--- src/component/FullScreenModalComponent.js
+++ src/component/FullScreenModalComponent.js
@@ -53,6 +53,15 @@ function FullScreenModalComponent({
           marginRight: "-50%",
           transform: "translate(-50%, -50%)",
         },
+        overlay: {
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0, 0, 0, 0.75)",
+          zIndex: 1000, // 提高z-index以确保覆盖全屏
+        },
       }}
     >
       {/* Modal的内容 */}
